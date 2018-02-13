87b99a5036f2a12ca29b9c5e67ce2ea8c39814ea
David McDermott <davidsamuelmcdermott@gmail.com>
2018-02-13 13:17:27 -0500

    Added comments to ping.bat
    Note: rem is the batch command remark, which is a commen (it's stupid, I know)

--- HackPSU Data Collection/ping.bat
+++ HackPSU Data Collection/ping.bat
@@ -1,8 +1,13 @@
 @echo off
+
+rem get the location that the user wants entered into the first cell
 set /P location=Please enter your location: 
 if "%location%"=="" GOTO Error
+
+rem run the python script.  update ws://localhost:5000/v1/pi to the server address
 python ping.py ws://localhost:5000/v1/pi 10 pings.csv %location%
 GOTO End
+
 :Error
 echo A location must be entered
 :End
