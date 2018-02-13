caaa75eef419e9b8fa46b5b8f14d8b9a17fb5c45
David McDermott <davidsamuelmcdermott@gmail.com>
2018-02-13 13:20:57 -0500

    Added comments to ping.sh

--- HackPSU Data Collection/ping.sh
+++ HackPSU Data Collection/ping.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+#Get the location that the user wants entered into the first cell
 read -p "Please enter your location: " location
 if [ "$location" == "" ]
 then
@@ -6,5 +7,7 @@ then
 	exit -1
 fi
 
+#Run ping.py using python (probably python2).  We need to update ws://localhost:5000/v1/pi to the actual server
 python ping.py ws://localhost:5000/v1/pi 10 pings.csv $location
+
 read -n1 -r -p "Press any key to continue..." key
